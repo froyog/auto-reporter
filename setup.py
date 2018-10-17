fa7592ecdf3c2585f8f8c04f996c76986b69b7ec
Tom <scarlet0345@gmail.com>
2018-10-17 23:15:16 +0800

    version 0.2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/froyog/auto-reporter'
 EMAIL = 'scarlet0345@gmail.com'
 AUTHOR = 'froyog'
 REQUIRES_PYTHON = '>=3.3.0'
-VERSION = '0.1.7'
+VERSION = '0.2.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
